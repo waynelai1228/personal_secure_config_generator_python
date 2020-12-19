0c179dbe8c7420cdb2272a4e60900733a91f6073
Wayne Lai <wayne128@attu7.cs.washington.edu>
2020-12-19 06:30:52 -0800

    Finish listing every setting in sshd config

--- sshd_config.py
+++ sshd_config.py
@@ -1,48 +1,30 @@
+import json
+
 class SSHDConfig:
-  # This structure holds all the setting names and permitted value
-  #
-  # For now, the options that are customizable are marked by 'String'
-  # The eventual plan is to create their own custom type and add pattern 
-  # validation
-  sshd_config_opt = {
-      'AcceptEnv': {'type': 'String'},
-      'AddressFamily': {'type': 'Single-Option', 'options': ('any', 'inet', 'inet6')},
-      'AllowAgentForwarding': {'type': 'Single-Option', 'options': ('yes', 'no')},
-      'AllowGroups': ('String'),
-      'AllowTcpForwarding': {'type': 'Single-Option', 'options': ('yes', 'no')},
-      'AllowUsers': {'type': 'String'},
-      'AuthorizedKeysFile': ('String'),
-      'ChallengeResponseAuthentication': ('Single-Option', 'yes', 'no'),
-      'ChrootDirectory': ('String'),
-      'Compression': ('Single-Option', 'delayed', 'yes', 'no'),
-      'DenyGroups': ('String'),
-      'DenyUsers': ('String'),
-      'ForceCommand': ('String'),
-      'GatewayPorts': ('String','127.0.0.1'),
-      'GSSAPIStoreCredentialsOnRekey': ('Single-Option', 'no', 'yes'),
-      'HostbasedUsesNameFromPacketOnly': ('Single-Option', 'no', 'yes'),
-      'HostKey': ('String','/etc/ssh/ssh_host_key'),
-      'IgnoreRhosts': ('Single-Option', 'yes', 'no'),
-      'IgnoreUserKnownHosts': ('Single-Option', 'no', 'yes'),
-    }
-  # Same as above, but this structure holds settings that are only
-  # available in version 2
-  sshd_config_opt_v2 = {
-      'Banner', ['String', 'none'],
-      'Ciphers': ('Multiple-Options', '[<array of index>]',
-                  '3des-cbc', 'aes128-cbc', 'aes192-cbc', 'aes256-cbc',
-                  'aes128-ctr', 'aes192-ctr', 'aes256-ctr', 'arcfour128',
-                  'arcfour256', 'arcfour', 'flowfish-cbc', 'cast128-cbc'),
-      'ClientAliveCountMax': ('Number', 3);
-      'ClientAliveInterval': ('Number', 0);
-      'GSSAPIAuthentication': ('Single-Option', 'no', 'yes'),
-      'GSSAPIKeyExchange': ('Single-Option', 'no', 'yes'),
-      'GSSAPICleanupCredentials': ('Single-Option', 'yes', 'no'),
-      'GSSAPIStrictAcceptorCheck': ('Single-Option', 'no', 'yes'),
-      'HostbasedAuthentication': ('Single-Option', 'no', 'yes'),
-      'HostKey': ('String','/etc/ssh/ssh_host_key'),
-  # This class holds a single configuration and its description
   def __init__(self, keyword, argument, description):
-    self.key = keyword
+    sshd_config_json = open("sshd_config_opt.json", "r")
+    sshd_config_v2_json = open("sshd_config_opt_v2.json", "r")
+    # This structure holds all the setting names and permitted value
+    #
+    # For now, the options that are customizable are marked by 'String'
+    # The eventual plan is to create their own custom type and add pattern 
+    # validation
+    self.sshd_config_opt = json.loads(sshd_config_json.read())
+    # Same as above, but this structure holds settings that are only
+    # available in version 2
+    # This class holds a single configuration and its description
+    self.sshd_config_opt_v2 = json.loads(sshd_config_v2_json.read())
+
+    # first check that the keyword is a valid setting
+    if keyword in self.sshd_config_opt:
+      # use the key to get to the type
+
+      # if type is string, then argument can be anything
+
+      # if type is single value, then argument must be on of the options
+
+      # if type is multi option, split the argument using delimiter ','
+      # and check that each individual value is valid
+      self.key = keyword
     self.arg = argument
     self.desc = description
